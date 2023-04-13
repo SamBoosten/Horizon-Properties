6cc50ab80e25085fb3f0eb87ecd46bd6b64aef11
Sam <boostensam@gmail.com>
2023-04-13 22:08:33 +0200

    content volledig toegevoegd

--- app/public/wp-content/themes/Horizon-Properties-custom/footer.php
+++ app/public/wp-content/themes/Horizon-Properties-custom/footer.php
@@ -1,10 +1,23 @@
-<footer class="footer text-center py-2 theme-bg-dark">
-		   
-            <p class="copyright"><a href="https://youtube.com/FollowAndrew">FollowAndrew</a></p>
-		   
-	    </footer>
-    
+    <footer class="footer text-center">
+        <div class="primary-overlay py-5">
+            <a href="/"><img class="" src="/wp-content/themes/Horizon-Properties-custom/assets/images/logo-footer.png" alt="logo"></a>	
+            <div class="footer-text pt-3">
+                <p>Kasteelstraat 41 | 3770 Riemst | BTW. BE 0568.452.980</p>
+                <div class=" text-center">
+                    <i class="fas fa-phone-alt"></i><a href="tel:00495232465"> +32 0495 23 24 65</a>
+                    <i class="fas fa-envelope pl-3"></i><a href="mailto:info@horizonproperties.com"> info@horizonproperties.com</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <div class="credentials d-flex justify-content-between pt-2 px-3">
+        <p>© Copyright Horizon Properties <?php echo date("Y")?> | <a href="#">Algemene voorwaarden</a> | <a href="#">Privacy Policy</a> | <a href="#">Cookies</a> </p>
+        <div class="socials">
+            <a href="https://www.facebook.com"><i class="fab fa-facebook-square"></i></a>
+        </div>
     </div>
+    
+</div>
 
 <?php
     wp_footer()
--- app/public/wp-content/themes/Horizon-Properties-custom/front-page.php
+++ app/public/wp-content/themes/Horizon-Properties-custom/front-page.php
@@ -1,40 +1,66 @@
 <?php
     get_header();
 ?>
-    <div class="main-wrapper">
+    <div class="main-wrapper ">
 	    <header class="page-intro text-center py-5">
             <img class="logo-tagline" src="/wp-content/themes/Horizon-Properties-custom/assets/images/logo-tagline.png" alt="logo">
 		</header>
-		<article class="content px-3 py-5 p-md-5">
-            <div class="usp text-center">
-                <h1>Waarom Horizon Properties?</h1>
-                <div class="horizon-usp text-left row">
-                    <ul class="col">
-                        <li>Ervaren professionals met diepgaande kennis van de vastgoedmarkt.</li>
-                        <li>Persoonlijke aandacht en maatwerk voor elke klant.</li>
-                    </ul>
-                    <ul class="col">
-                        <li>Uitgebreid netwerk van vastgoedprofessionals voor de beste deals en kansen.</li>
-                        <li>Innovatieve technologieën en tools om klanten te helpen bij het nemen van weloverwogen beslissingen.</li>
-                    </ul>
+		<article class="content">
+            <div class="usp">
+                <div class="primary-overlay text-center px-3 py-5 p-md-5">
+                    <h1>Waarom HORIZON PROPERTIES?</h1>
+                    <div class="horizon-usp text-left row">
+                        <ul class="col">
+                            <li>Ervaren professionals met diepgaande kennis van de vastgoedmarkt.</li>
+                            <li>Persoonlijke aandacht en maatwerk voor elke klant.</li>
+                        </ul>
+                        <ul class="col">
+                            <li>Uitgebreid netwerk van vastgoedprofessionals voor de beste deals en kansen.</li>
+                            <li>Innovatieve technologieën en tools om klanten te helpen bij het nemen van weloverwogen beslissingen.</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-            <div class="projecten">
-                <div class="kaarten">
+            <div class="projecten-block">
+                <div class="white-overlay px-3 py-5 p-md-5 text-center">
+                    <h1>Populair op dit moment</h1>
+                    <div class="kaarten">
 
-                </div>
-                <div class="d-flex">
-                    <a href="#" class="btn">Te koop</a>
+                    </div>
+                    <div class="d-flex justify-content-center py-3">
+                        <a href="#" class="knop-klein">Te koop</a>
+                        <a href="#" class="knop-klein">Te huur</a>
+                    </div>
                 </div>
             </div>   
-            <?php
-                if( have_posts() ) {
-                    while( have_posts() ) {
-                        the_post();
-                        the_content();
-                    }
-                }
-            ?>
+            <div class="over-ons-block">
+                <div class="primary-overlay px-3 py-5 p-md-5 text-center">
+                    <h1>Wie zijn we?</h1>
+                    <p class="over-ons-text">Horizon Properties bestaat uit een team van ervaren en goed opgeleide vastgoedprofessionals die zich richten op het helpen van klanten bij het kopen, verkopen of investeren in vastgoed. Het bedrijf biedt persoonlijke aandacht en maatwerk voor elke klant, heeft een uitgebreid netwerk van vastgoedprofessionals en maakt gebruik van innovatieve technologieën en tools om klanten te helpen bij het nemen van weloverwogen beslissingen. Met diepgaande kennis van de vastgoedmarkt streeft dit bedrijf ernaar om de horizon van hun klanten te verbreden en hun droomhuis te vinden.</p>
+                    <div class="fotos">
+                        <div class="foto-boven">
+                            
+                        </div>
+                        <div class="foto-onder">
+                                
+                        </div>
+                        <div class="foto-boven">
+                                    
+                        </div>
+                        <div class="foto-onder">
+                                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="projecten-block">
+                <div class="white-overlay px-3 py-5 p-md-5 text-center">
+                    <h1>Wil jij jouw woning laten schatten?</h1>
+                    <div class="pt-4">
+                        <a href="#" class="knop-groot">vraag vrijblijvend een offerte aan</a>
+                    </div>
+                </div>
+            </div>
 	    </article>    
 <?php
     get_footer();
